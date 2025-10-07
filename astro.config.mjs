469a097d9ffaa926501461a04509e81d4ec9659d
YerayYas <84556441+yeray-yas@users.noreply.github.com>
2025-10-07 23:13:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennisklappe.github.io',
+  site: 'https://yeray-yas.github.io',
   // Only use base path in production (GitHub Pages)
   base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
   integrations: [sitemap()],
